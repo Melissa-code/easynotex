08c34709436195e5759852e3ea5f5f29fdff18e1
Melissa Benard <benardmeli@yahoo.fr>
2025-02-14 11:51:09 +0100

    Define css variables

---
+++ frontend/src/components/NotesComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="">
+                <h1>Mes notes</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'NotesComponent',
+        data() {
+            return {
+                //
+            };
+        },
+       
+    };
+</script>
+
+<style scoped>
+   
+</style>
