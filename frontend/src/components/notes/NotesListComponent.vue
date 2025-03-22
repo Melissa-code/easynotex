6ec4dfd47888d6a5b6b3636376990523031c47af
Melissa Benard <benardmeli@yahoo.fr>
2025-03-22 19:24:41 +0100

    feat: add a loading spinner while fetching notes

--- frontend/src/components/notes/NotesListComponent.vue
+++ frontend/src/components/notes/NotesListComponent.vue
@@ -1,24 +1,48 @@
 <script>
 import NoteCardComponent from './NoteCardComponent.vue';
+import Spinner from './Spinner.vue';
 
 export default {
     name: 'NotesListComponent',
     props: {
         notes: {
             type: Array,
-            default: () => [] 
+            default: () => []
         }
     },
+    data() {
+        return {
+            // start loading state
+            loading: true,
+        };
+    },
     components: {
-        NoteCardComponent
-    }
+        NoteCardComponent,
+        Spinner,
+    },
+    watch: {
+        notes: {
+            handler() {
+                // When notes are loaded, change state to true (spinner)
+                if (this.notes.length > 0) {
+                    this.loading = false; 
+                }
+            },
+            immediate: true,
+        },
+    },
 };
 </script>
 
 <template>
     <div class="mx-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-10">
+        <!-- spinner -->
+        <div v-if="loading" class="col-span-1 flex justify-center items-center">
+            <Spinner /> 
+        </div>
+
         <!-- No note found -->
-        <div v-if="notes.length === 0"
+        <div v-else-if="notes.length === 0"
             class="bg-white rounded-2xl border border-[--dark-green] overflow-hidden col-span-1">
             <!-- Title -->
             <div class="p-4 border-b border-b-[--dark-green] bg-[--yellow-light] text-center">
@@ -27,9 +51,9 @@ export default {
             <!-- Infos -->
             <div class="p-4">
                 <ul>
-                    <br><br>
+                    <br/><br/>
                     <li class="mb-4 font-semibold">Pas de contenu disponible</li>
-                    <br /><br /><br /><br />
+                    <br/><br/><br/><br/><br/>
                 </ul>
             </div>
         </div>
---
+++ frontend/src/components/notes/Spinner.vue
@@ -0,0 +1,34 @@
+<script>
+  export default {
+    name: 'Spinner',
+  };
+</script>
+
+<template>
+  <div class="spinner">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style scoped>
+ .spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 33vh;
+}
+
+.loader {
+  border: 8px solid #1A535C; 
+  border-top: 8px solid #FFE66D;
+  border-radius: 50%;
+  width: 80px;  
+  height: 80px; 
+  animation: spin 1.5s linear infinite; 
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
